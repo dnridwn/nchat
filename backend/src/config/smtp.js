39e10c75e54e966f275f4f8344d95442f5db9d87
dnridwn <denridwan123@gmail.com>
2023-01-28 05:35:32 +0700

    update smtp configuration

--- backend/src/config/smtp.js
+++ backend/src/config/smtp.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export default {
+    SMTP_DRIVER: process.env.SMTP_DRIVER,
     SMTP_HOST: process.env.SMTP_HOST,
     SMTP_PORT: process.env.SMTP_PORT,
     SMTP_USERNAME: process.env.SMTP_USERNAME,
