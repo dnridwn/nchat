a236e15e6cb6ad9f68aa5d3f2ef3e755ac94db8c
dnridwn <denridwn123@gmail.com>
2023-01-28 04:29:30 +0700

    refactor database.js

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,15 +1,15 @@
 import mongoose from 'mongoose'
 import databaseConfig from '../config/database.js'
 
-export default {
-    setup: function() {
-        mongoose.connect(`mongodb://${databaseConfig.DB_HOST}:${databaseConfig.DB_PORT}/${databaseConfig.DB_NAME}`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
-        
-        const db = mongoose.connection
-        db.on('error', (error) => console.error(error))
-        db.once('open', () => console.log('Database connected'))
-    }
+const setup = function() {
+    mongoose.connect(`mongodb://${databaseConfig.DB_HOST}:${databaseConfig.DB_PORT}/${databaseConfig.DB_NAME}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    
+    const db = mongoose.connection
+    db.on('error', (error) => console.error(error))
+    db.once('open', () => console.log('Database connected'))
 }
+
+export default { setup }
