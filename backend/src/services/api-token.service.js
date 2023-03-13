08f964e11ee85aa5efc532d9d637b039c6491674
dnridwn <denridwan123@gmail.com>
2023-03-14 00:12:23 +0700

    update api token create query

--- backend/src/services/api-token.service.js
+++ backend/src/services/api-token.service.js
@@ -6,12 +6,10 @@ export default class ApiTokenService {
     static async create(user) {
         return new Promise(async (resolve, reject) => {
             const token = crypto.randomBytes(20).toString('hex')
-            const apiToken = await ApiTokenModel.create({ token, user })
-            if (apiToken == undefined || apiToken == null || apiToken.length == 0) {
-                reject('Failed to create api token!')
-            }
-            
-            resolve(apiToken)
+            ApiTokenModel.create({ token, user }, (err, result) => {
+                if (err) reject(err)
+                resolve(result)
+            })
         })
     }
 }
